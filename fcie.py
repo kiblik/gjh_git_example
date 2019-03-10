42d064504111b78a65dec3c3146dfc100b0b1b03
Tomas Kubla <tomas@kubla.sk>
2019-03-11 00:56:33 +0100

    x^2, sin(2x), sin(x+pi)

--- fcie.py
+++ fcie.py
@@ -6,4 +6,11 @@ def sinx(x):
 def cosx(x):
     return math.cos(x)
 
+def xnadruhu(x):
+    return x**2
 
+def sindvax(x):
+    return 2*math.sin(x)
+
+def sinxpluspi(x):
+    return math.sin(x)+math.pi
